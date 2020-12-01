005999a229969ef063fb049d3c5bbf297bdb7258
cscl-git <57174349+cscl-git@users.noreply.github.com>
2020-12-01 04:53:18 +0000

    updated bookins code

--- bookings/src/main/resources/db/migration/main/V20200809140535__bookings_alter_ddl.sql
+++
@@ -1,8 +0,0 @@
-
-alter table public.bk_bookings drop column fromtime;
-
-alter table public.bk_bookings drop column totime;
-
-alter table public.bk_bookings drop column bk_from_time;
-
-alter table public.bk_bookings drop column bk_to_time;
